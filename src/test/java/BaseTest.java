22e56547ba10fb227d8dc82925166ba463c22bb8
Furkan Zambak <furkan.zambak@eplatform.com.tr>
2022-12-23 16:37:19 +0300

    Disable notifications flags added for browsers

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -2,11 +2,15 @@ import com.thoughtworks.gauge.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import util.PropertyManager;
 
 import java.time.Duration;
+import java.util.HashMap;
 
 public class BaseTest {
     public static WebDriver driver;
@@ -15,15 +19,23 @@ public class BaseTest {
     public void setUp() {
         if(PropertyManager.getInstance().getBrowserType().equals("Chrome")) {
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+            ChromeOptions p = new ChromeOptions();
+            p.addArguments("--disable-notifications");
+            driver = new ChromeDriver(p);
         }
         else if(PropertyManager.getInstance().getBrowserType().equals("Firefox")) {
             WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
+            FirefoxOptions p = new FirefoxOptions();
+            p.addPreference("dom.webnotifications.enabled", false);
+            driver = new FirefoxDriver(p);
         }
         else if(PropertyManager.getInstance().getBrowserType().equals("MSEdge")) {
             WebDriverManager.edgedriver().setup();
-            driver = new EdgeDriver();
+            HashMap<String, Object> edgePrefs = new HashMap<String, Object>();
+            edgePrefs.put("profile.default_content_settings.popups", 0);
+            EdgeOptions edgeOptions = new EdgeOptions();
+            edgeOptions.setExperimentalOption("prefs",edgePrefs);
+            driver = new EdgeDriver(edgeOptions);
         }
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(PropertyManager.getInstance().getImplicitWaitDuration()));
@@ -31,10 +43,6 @@ public class BaseTest {
 
     @AfterSuite
     public void tearDown() {
-        //driver.quit();
+        driver.quit();
     }
-
-    /*public WebDriver getDriver() {
-        return driver;
-    }*/
 }
